9fdddf1212705b96cdeaa3728e5f6e72a2ad3c80
Timothy Collins <timothycollins.sean@gmail.com>
2021-08-26 23:13:58 +0500

    Removed body-parser as express has inbuilt body-parser

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
-app.use(bodyParser.json()); //application/json
+app.use(express.json()); //application/json
 
 app.use('/feed', feedRoutes);
 
