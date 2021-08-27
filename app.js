0db43d358dddf92e07196c76719e92eed35a61b6
Timothy Collins <timothycollins.sean@gmail.com>
2021-08-27 23:38:04 +0500

    Added headers for API clients CORS errors

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const app = express();
 
 app.use(express.json()); //application/json
 
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();    
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
